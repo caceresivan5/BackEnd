5ee2a747ed63fc42e79581b72e5e5560dbe4e7b5
Ivan Caceres <dg2.ivancaceres@gmail.com>
2022-01-28 03:02:21 -0300

    desafio2 funciona perfecto

--- desafio2.js
+++ desafio2.js
@@ -1,13 +1,14 @@
-import fs from "fs";
+
+const fs = require('fs');
 
 class File {
   constructor() {
-    this.filePath = "./products.txt";
+    this.filePath = "products.txt";
   }
   async read() {
     try {
-      const users = await fs.promises.readFile(this.filePath, "utf-8");
-      return JSON.parse(users);
+      const file = await fs.promises.readFile(this.filePath, "utf-8");
+      return JSON.parse(file);
     } catch (err) {
       return [];
     }
@@ -26,14 +27,14 @@ class File {
         this.filePath,
         JSON.stringify(products, null, 2)
       );
-      return `Added ${title} to the product list`;
+      return `AGREGADO ${title} A LA LISTA DE PRODUCTOS`;
     } catch (err) {
-      console.log("Something went wrong", err);
+      console.log("ALGO SALIO MAL", err);
     }
   }
   async delete() {
     await fs.promises.unlink(this.filePath);
-    console.log('Deleted')
+    console.log('ELIMINADO')
   }
 }
 const main = async () => {
@@ -41,16 +42,23 @@ const main = async () => {
   console.log("Read: ", await fileHandler.read());
   console.log(
     await fileHandler.save(
-      "Mercedes Benz C Class",
-      48000,
-      "https://i.blogs.es/4ba188/mercedes-benz-c-class-2019-1600-01/1366_2000.jpg"
+      "PlayStation 1 ",
+      60000,
+     "https://static.turbosquid.com/Preview/2017/09/24__12_23_05/CONTEXTIMAGE.jpg0187D165-443E-441A-8E39-C55006D589A3Large.jpg"
+      )
+  );
+  console.log(
+    await fileHandler.save(
+      "PlayStation 4",
+      70000,
+      "https://m.media-amazon.com/images/I/6118ctEjpoL._AC_SL1500_.jpg"
     )
   );
   console.log(
     await fileHandler.save(
-      "BMW Serie 3",
-      45000,
-      "https://hips.hearstapps.com/es.h-cdn.co/cades/contenidos/9888/bmw-serie-3-2018-delantera.jpg?resize=980:*"
+      "PlayStation 5",
+      90000,
+      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/ps5-playstaton-5-2-1600255876.jpg"
     )
   );
   console.log("Read: ", await fileHandler.read());
