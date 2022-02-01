9467a14a2559634ed2d8d3fedcaccd169cd14e77
Ivan Caceres <dg2.ivancaceres@gmail.com>
2022-02-01 20:57:41 -0300

    preparando la carpeta clase 8

---
+++ clase8/index8.js
@@ -0,0 +1,20 @@
+/* 
+1 creo una carpeta
+ 
+2 en la terminal escribo npm init -y  para crear un package Json
+(me ubico en la carpeta con cd nombre de la carpeta / )
+
+3 creo el index.js
+
+4 si voy a utilizar express debo instalarlo
+( npm install --save express )
+
+5 instalo NODEMON (live server en frontend) 
+( npm install --save nodemon)
+
+
+6 verifico todo en mi package.json si esta todo ok
+
+7 en el package.json voy a script - creo start -- "start": "nodemon index.js"
+
+ */
